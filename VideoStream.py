ae1378c4d12bb01f7c5571d90a9c89d61225f67e
NguyenThienAn0610 <68161545+NguyenThienAn0610@users.noreply.github.com>
2021-04-11 11:31:37 +0700

    Update VideoStream.py

--- VideoStream.py
+++ VideoStream.py
@@ -11,27 +11,16 @@ class VideoStream:
 
     def nextFrame(self):
         """Get next frame."""
-
         data = self.file.read(5)  # Get the framelength from the first 5 bytes
-        # data_ints = struct.unpack('<' + 'B'*len(data),data)
         data = bytearray(data)
-
-        # data_int = (data[0] - 48) * 10000 + (data[1] - 48) * 1000 + (data[2] - 48) * 100 + (data[3] - 48) * 10 + (
-        #         data[4] - 48)  # = #int(data.encode('hex'),16)
-
-        # final_data_int = data_int
-
         if data:
-
-            framelength = int(data)#final_data_int  # int(data)#final_data_int/8  # xx bytes
+            framelength = int(data)#final_data_int/8  # xx bytes
             # Read the current frame
             frame = self.file.read(framelength)
             if len(frame) != framelength:
                 raise ValueError('incomplete frame data')
-
             self.frameNum += 1
             print('-' * 10 + "\nNext Frame (#" + str(self.frameNum) + ") length:" + str(framelength) + "\n" + '-' * 10)
-
             return frame
 
     def frameNbr(self):
